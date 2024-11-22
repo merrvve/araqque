93279a7baaf09f65c4b60c931bbf1e277d83ee0b
mervs <mervsen@gmail.com>
2024-11-22 12:27:13 +0300

    firebase

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,7 +1,19 @@
 // lib/firebaseAdmin.js
 import admin from 'firebase-admin';
 import { initializeApp, getApps, cert, ServiceAccount } from 'firebase-admin/app';
-import serviceAccount from './firebase-adminsdk.json';
+//import serviceAccount from './firebase-adminsdk.json';
+const firebaseConfig = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
 export const verifyIdToken = async (token: any) => {
   try {
     return await admin.auth().verifyIdToken(token);
@@ -13,7 +25,7 @@ export const verifyIdToken = async (token: any) => {
 
 
 const firebaseAdminConfig = {
-    credential: admin.credential.cert(serviceAccount as ServiceAccount),
+    credential: admin.credential.cert(firebaseConfig as ServiceAccount),
       //databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
       //storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 }
